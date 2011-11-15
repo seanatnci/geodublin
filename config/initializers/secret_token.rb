7f78ca9e466fae7c736d58cad27e4c8a9a104542
Sean Cahill <sean@nmm.ie>
2011-11-15 21:03:03 +0000

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geodublin::Application.config.secret_token = '470b1b1b964bb53efceb8b562256e1a7736375ee22e695c02f1f3a8783a51da38146b2b1966b821d1b211ba955c3fe767d2048dbe5b4083d9820b3e04cbccbf3'
