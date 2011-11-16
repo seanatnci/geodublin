d816d4714a950608f78b85a806f46e9cd95b7352
Sean Cahill <sean@nmm.ie>
2011-11-16 16:08:48 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Geodublin::Application.routes.draw do
+  get "locations/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Geodublin::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "locations#index"
 
   # See how all your routes lay out with "rake routes"
 
